2533b4c77ea7befb3abc6444aea179f08a2e06cc
Leon Verhelst <verhelst@unbc.ca>
2014-02-09 21:39:37 -0800

    Initial Commit

---
+++ course-advisor/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author Leon "The Night" Verhelst
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Application should be completed with the below line!! 
+        // 10/10 would code again
+        System.out.println("Emery Smells!");
+    }
+    
+}
