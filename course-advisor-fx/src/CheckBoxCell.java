f901d293fc732db75ff328cde63daecf21612fb5
U-BIRG\eberg0 <eberg0@ws-31.birg.UNBC.ca>
2014-02-25 11:51:48 -0800

    added rulelist: contains parsing of the rules and intersection methods

--- course-advisor-fx/src/CheckBoxCell.java
+++ course-advisor-fx/src/CheckBoxCell.java
@@ -1,4 +1,3 @@
-
 import javafx.scene.control.ListCell;
 
 /**
--- course-advisor-fx/src/Main.java
+++ course-advisor-fx/src/Main.java
@@ -8,13 +8,15 @@ public class Main {
      */
     public static void main(String[] args) {
         CourseList cl = new CourseList();
-        if(cl.loadCourseList()){
-            Session session = new Session(5);
+        RuleList rl = new RuleList();
+        
+        if(cl.loadCourseList() && rl.loadRuleList("cpscrules.txt")){
             Course[] initialCourses = cl.get("CPSC100","CPSC141","MATH100","CPSC242", "MATH220");
-          
+                        
+            Session session = new Session(5);          
             session.setInitialCourses(initialCourses);
                         
-            InferenceEngine ie = new InferenceEngine(session, cl);
+            InferenceEngine ie = new InferenceEngine(session, cl, rl);
             session = ie.infer();
             
             System.out.println("Credit hours: " + session.credit_hours + "/120 = " + (float)session.credit_hours/120 * 100 + "%");
