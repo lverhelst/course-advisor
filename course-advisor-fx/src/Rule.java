bb022c38c3419b0369c2c057b3fc6b742be046ad
Emery Berg <emery.berg@gmail.com>
2014-02-25 16:36:38 -0800

    yeah forgot to add again...

---
+++ course-advisor-fx/src/Rule.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+/**
+ * Interface for the rule class
+ * @author Emery
+ */
+public interface Rule {
+    /**
+    * Simple method which returns true if the rule passes
+    * @param set the set of courses to check against the list
+    * @return true if the rule passes
+    */
+   public boolean check(String[] set);
+
+   /**
+    * Used to get the intersection of 2 list of courses
+    * @param set the set of courses to compare against
+    * @return the list of courses which match (null if no type is invalid)
+    */
+   public ArrayList<String> intersect(String[] set);
+   
+   /**
+    * @return a string of the rule type 
+    */
+   public String getType();
+
+   /**
+    * @return the name of the rule
+    */
+   public String getAction();
+}
